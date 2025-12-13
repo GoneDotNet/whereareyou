936b0736e9390af916d6dac46e25fd17ee3a3d3d
Allan Ritchie <allan.ritchie@gmail.com>
2025-12-13 17:36:41 -0500

    Fixes to migrations and nuget bumps

--- GoneDotNet.WhereAreYou.DbMigrations/Program.cs
+++ GoneDotNet.WhereAreYou.DbMigrations/Program.cs
@@ -1,8 +1,28 @@
-using GoneDotNet.WhereAreYou.DbMigrations;
+using GoneDotNet.WhereAreYou.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.AddAppDbContext();
-builder.Services.AddHostedService<Worker>();
 
-var host = builder.Build();
-host.Run();
+builder.AddServiceDefaults();
+builder.AddAppDbContext();
+
+var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+var strategy = dbContext.Database.CreateExecutionStrategy();
+await strategy.ExecuteAsync(async () =>
+{
+    Console.WriteLine("Dropping and recreating the database...");
+    
+    var dbExistedBeforeMigration = await dbContext.Database.CanConnectAsync();
+    await dbContext.Database.MigrateAsync();
+    if (!dbExistedBeforeMigration)
+    {
+        Console.WriteLine("Seeding initial data...");
+        // await DataSeeder.SeedDataAsync(dbContext);
+    }
+});
+Console.WriteLine("Db migration completed");
